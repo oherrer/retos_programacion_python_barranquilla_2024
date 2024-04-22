9057afc0a82a5e6f9b98659bdeab0c47aa78d5e3
oherrer <167105937+oherrer@users.noreply.github.com>
2024-04-22 17:22:30 +0000

    reto 04

---
+++ retos/reto_04/soluciones/oherrer.py
@@ -0,0 +1,22 @@
+def incremento(n):
+    return n+1
+
+def decremento(n):
+    return n-1
+
+
+def suma (n , m):
+    for i in range (m):
+        n = incremento(n)
+    return n 
+def resta (n , m):
+    for i in range (m):
+        n= decremento(n)
+    return -n    
+
+
+def multiplicacion (n , m):
+    for i in range (m):
+        
+
+if __name__ = "__main":

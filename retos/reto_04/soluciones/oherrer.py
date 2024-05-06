b2936799ae817aad8b3ea326252896fd54f97008
oherrer <167105937+oherrer@users.noreply.github.com>
2024-05-06 15:01:50 +0000

    agregando la funcion dividendo

--- retos/reto_04/soluciones/oherrer.py
+++ retos/reto_04/soluciones/oherrer.py
@@ -16,30 +16,31 @@ def resta (n , m):
 
 
 def multiplicacion(n, m):
-  """
-  Esta función calcula la multiplicación de un número 'n' por otro número 'm'
-  utilizando la función 'suma' repetidamente.
+    """
+    Esta función calcula la multiplicación de un número 'n' por otro número 'm'
+    utilizando la función 'suma' repetidamente.
 
-  Parámetros:
-    n (int): El número que se va a multiplicar.
-    m (int): El número de veces que se va a sumar 'n' a sí mismo.
+    Parámetros:
+        n (int): El número que se va a multiplicar.
+        m (int): El número de veces que se va a sumar 'n' a sí mismo.
 
-  Retorno:
-    int: El resultado de la multiplicación 'n * m'.
-  """
+    Retorno:
+        int: El resultado de la multiplicación 'n * m'.
+    """
 
-  if m == 0:  # Caso base: si m es 0, la multiplicación es 0.
-    return 0
+    if m == 0:  # Caso base: si m es 0, la multiplicación es 0.
+        return 0
 
-  else:  # Caso recursivo: si m es mayor que 0, se suma 'n' a sí mismo 
-       # 'm' veces y se devuelve el resultado.
-    return n + multiplicacion(n, m - 1)
+    else: 
+        # Caso recursivo: si m es mayor que 0, se suma 'n' a sí mismo 
+        # 'm' veces y se devuelve el resultado.
+        return n + multiplicacion(n, m - 1)
 
-# Ejemplo de uso
-resultado = multiplicacion(9, 9)
-print(f"9 multiplicado por 9 usando la función suma es: {resultado}")  
+
 
         
 
-if __name__ = "__main":
-    pass
+if __name__ = "__main__":
+    # Ejemplo de uso
+    resultado = multiplicacion(9, 9)
+    print(f"9 multiplicado por 9 usando la función suma es: {resultado}") 
